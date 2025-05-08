352afffdbcb171e37a905948d0a5bfb37d807ea3
sivajik <sivaji_kondapalli@yahoo.com>
2025-05-08 16:20:41 +0100

    using completablefuture

--- src/main/java/com/jingineering/multithreading/abhayani/OneHundresMillionPrimesV2UnfairThreads.java
+++ src/main/java/com/jingineering/multithreading/abhayani/OneHundresMillionPrimesV2UnfairThreads.java
@@ -2,6 +2,7 @@ package com.jingineering.multithreading.abhayani;
 
 import java.time.Duration;
 import java.time.LocalTime;
+import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
@@ -17,7 +18,7 @@ v2) for 100000000 numbers we have: 5761455 primes & took PT6.738088S
 public class OneHundresMillionPrimesV2UnfairThreads {
     public static void main(String[] args) throws InterruptedException {
         LocalTime start = LocalTime.now();
-        int limit = 100_000;
+        int limit = 100_000_000;
         System.out.println("v2) for " + limit + " numbers we have: " + getTotalPrimesUntil(limit)
                 + " primes & took " + Duration.between(start, LocalTime.now()));
     }
@@ -26,15 +27,17 @@ public class OneHundresMillionPrimesV2UnfairThreads {
         AtomicInteger totalPrimeCount = new AtomicInteger(0);
 
         int totalThreads = 10;
-        int batchSize = limit/totalThreads;
+        int batchSize = limit / totalThreads;
 
         ExecutorService es = Executors.newFixedThreadPool(totalThreads);
+        CompletableFuture<String>[] list = new CompletableFuture[totalThreads];
+
 
         for (int i = 0; i < totalThreads; i++) {
             int batchStart = i * batchSize + 1;
             int batchEnd = batchStart + batchSize - 1;
 
-            es.execute(() -> {
+            list[i] = CompletableFuture.supplyAsync(() -> {
                 LocalTime start = LocalTime.now();
                 for (int currNum = batchStart; currNum <= batchEnd; currNum++) {
                     if (isPrime(currNum)) {
@@ -44,12 +47,24 @@ public class OneHundresMillionPrimesV2UnfairThreads {
                 System.out.println("Thread: [" + Thread.currentThread().getName() + "] for range: ["
                         + batchStart + " -> " + batchEnd + "] found: " + totalPrimeCount.get()
                         + " took: " + Duration.between(start, LocalTime.now()));
-            });
+                return "";
+            }, es);
+        }
+
+        try {
+            CompletableFuture.allOf(list).join();/*.thenAccept(i -> {
+                for (var x : list) {
+                    var result = x.join();
+                    System.out.println(result);
+                }
+            });*/
+            System.out.println("All Tasks/threads are completed their execution...");
+        } catch (Exception e) {
+            e.printStackTrace();
         }
 
         es.shutdown();
-        es.awaitTermination(60, TimeUnit.SECONDS);
-
+        es.awaitTermination(100, TimeUnit.SECONDS);
         return totalPrimeCount.get();
     }
 
